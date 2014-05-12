ea655884ffa0cfdfc81e739c7992c0a62253bc46
Dominikdsgnr <Dominikdsgnr@gmail.com>
2014-05-12 15:57:19 +0200

    FB like lang fix

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
 	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.0";
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
